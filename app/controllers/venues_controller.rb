819f85cd49ae94aceadf73865f91dd364d62fd9a
ekatsnelson <elisek222@gmail.com>
2022-11-08 23:30:07 +0000

    First few changes

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,4 @@
 class VenuesController < ApplicationController
-
   def index
     matching_venues = venue.all
     venues = matching_venues.order(:created_at)
@@ -17,23 +16,23 @@ class VenuesController < ApplicationController
 
   def create
     @venue = Venue.new
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
-    venue.save
+    @venue.address = params.fetch("query_address")
+    @venue.name = params.fetch("name")
+    @venue.neighborhood = params.fetch("neighborhood")
+    @venue.save
 
-    redirect_to("/venues/#{venue.name}")
+    redirect_to("/venues/#{@venue.name}")
   end
-  
+
   def update
     the_id = params.fetch("venue_id")
 
     @venue = Venue.where({ :id => the_id })
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
-    
+
     redirect_to("/venues/#{venue.id}")
   end
 
@@ -45,5 +44,4 @@ class VenuesController < ApplicationController
 
     redirect_to("/venues")
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-  get("/", { :controller => "users", :action => "home" })
+  get("/", { :controller => "users", :action => "index" })
   get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
-  get("/insert_user_record", { :controller => "venues", :action => "create" })
+  get("/users/:username", { :controller => "users", :action => "show" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
   get("/update_users/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
@@ -11,7 +10,6 @@ Rails.application.routes.draw do
   get("/venue/:an_id", { :controller => "venue", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
-  
+
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
- 
 end
